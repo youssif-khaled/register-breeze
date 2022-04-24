bf9f2b6b9b0142cd555415a795a0a9a2f6c1e3c5
Youssif Khaled <youssifk390@gmail.com>
2022-04-24 16:19:32 +0200

    breeze

---
+++ database/migrations/2022_03_25_084144_create_stores_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table ->string('name',100);
+            $table ->string('email',100);
+            $table ->string('password',100);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stores');
+    }
+};
